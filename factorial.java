727f0c0bd3e038031979e14d6e16ff8d80e89001
veeravel87 <60774489+veeravel87@users.noreply.github.com>
2021-10-03 13:06:11 +0530

    Pgm for Factorial

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package Week1.day1;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int fact=1,n=5;
+for (int i =5; i > 0; i--) {
+	fact=fact*i;
+			
+	
+}
+System.out.println(fact);
+	}
+
+}
